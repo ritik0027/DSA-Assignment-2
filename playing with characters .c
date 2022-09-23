f9dda5225ebbdfc24c84e1685b4a2eee6c31e5d5
ritik0027 <114002245+ritik0027@users.noreply.github.com>
2022-09-23 16:27:33 +0530

    Create playing with characters .c

---
+++ playing with characters .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{ char ch, s[10] , sen[20] ;
+scanf("%c",&ch);
+scanf("%s\n",&s);
+gets(sen);
+printf("%c",ch);
+printf("\n%s",s);
+printf("\n%s",sen);
+
+   
+    return 0;
+}
